2ac8fb85503cdb1384e6d1732ecd5852c5b9ee12
Baranouski Raman <baranouski.raman@gmail.com>
2023-09-11 16:44:22 +0300

    remove unnecessary comment

--- src/main/java/by/teachmeskills/springbootproject/services/implementation/UserPrincipalDetailsService.java
+++ src/main/java/by/teachmeskills/springbootproject/services/implementation/UserPrincipalDetailsService.java
@@ -1,6 +1,5 @@
 package by.teachmeskills.springbootproject.services.implementation;
 
-import by.teachmeskills.springbootproject.entities.Role;
 import by.teachmeskills.springbootproject.entities.User;
 import by.teachmeskills.springbootproject.principal.UserPrincipal;
 import by.teachmeskills.springbootproject.repositories.UserRepository;
@@ -12,15 +11,6 @@ import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
-//There are two methods for defining permission - hasRole and hasAuthority.
-//The first doesn't require ROLE_ prefix.
-//The second requires it so requestMatchers.hasRole("ADMIN") is equal to requestMatchers.hasAuthority("ROLE_ADMIN")
-//But they do the same things.
-
-
-//UserDetails details = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-//@PreAuthorize() - поновее @RolesAllowed @Secured - постарее
-
 @Service
 @RequiredArgsConstructor
 public class UserPrincipalDetailsService implements UserDetailsService {
@@ -30,7 +20,6 @@ public class UserPrincipalDetailsService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Optional<User> user = userRepository.findByEmail(username);
         if (user.isPresent()) {
-            user.get().getRoles().add(Role.builder().id(0).name("ROLE_USER").build());
             return new UserPrincipal(user.get());
         } else {
             throw new UsernameNotFoundException("User wasn't found");
